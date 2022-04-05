7710bd061a14a002b151da50f6f1742ee6b5c8da
Carlos González <usan30509@gmail.com>
2022-04-05 04:16:12 -0500

    Commit de 3 archivos

---
+++ apps.js
@@ -0,0 +1,27 @@
+//Estoy dando variables
+
+var nombre = "Jamile";
+var nombre1 = "Eliane";
+var apellido = "Yucra";
+var apellido1 = "Briones";
+var edad = 21;
+var cumpleaños = "20 de Febrero";
+
+var nombre2 = "Carlos";
+var nombre3 = "David"
+
+//Pongo a prueba los datos tipo Boolean
+
+1 > 2;
+nombre2 == nombre2;
+nombre3 != nombre2;
+nombre3 == nombre2;
+
+//Uso el condicional if para ponerlo a prueba
+
+if (edad > 18) {
+    console.log("Puedes conducir " + apellido + " " + nombre)
+} else {
+    console.log("No puedes conducir " + apellido + " " + nombre)
+}
+
